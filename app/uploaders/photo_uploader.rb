9a1751a23a3995136bbd64075d51b8bda31b5df9
laury meredith <meredith.laury@gmail.com>
2017-08-18 17:18:09 +0100

    a bit of cleaning for production

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,4 +2,15 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
 
+  process eager: true
+
+  process convert: 'jpg'
+
+  version :card do
+    resize_to_fit 650, 400
+  end
+
+  version :cocktail_display do
+    cloudinary_transformation width: 20, height: 35, crop: :fit
+  end
 end
