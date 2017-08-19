3d03e56c5966375a8a5b88b9dd801f083f6f202a
laury meredith <meredith.laury@gmail.com>
2017-08-19 11:31:44 +0100

    makes cocktail image as a background image and cleaning the structure on the show

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -11,6 +11,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :cocktail_display do
-    cloudinary_transformation width: 350, height: 180, crop: :lfill, gravity: :center #, effect: :improve
+    cloudinary_transformation width: 270, height: 280, crop: :fill, gravity: :center #, effect: :improve
   end
 end
