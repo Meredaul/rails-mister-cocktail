11acca1dad451392b3a8cc17f1687f06935fedf7
laury meredith <meredith.laury@gmail.com>
2017-08-22 19:07:45 +0100

    gets the facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root to: 'pages#home'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root 'pages#home'
 
   # resources :users do
     # resources :cocktails, only: [:index, :create]
