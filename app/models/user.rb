5ac21abcb0d5d7d81e8a4687ca6173b4be1f89a6
laury meredith <meredith.laury@gmail.com>
2017-08-22 22:28:37 +0100

    nearly finished the google sign in, but issue with the redirection

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :cocktails, dependent: :nullify
 
-  devise :omniauthable, omniauth_providers: [:facebook]
+  devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
@@ -19,7 +19,7 @@ class User < ApplicationRecord
     user = User.find_by(provider: auth.provider, uid: auth.uid)
     user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
     if user
-      user.update(user_params)
+      #user.update(user_params)
     else
       user = User.new(user_params)
       user.password = Devise.friendly_token[0,20]  # Fake password for validation
@@ -29,4 +29,21 @@ class User < ApplicationRecord
     return user
   end
 
+  def self.from_omniauth(auth)
+    user_params = auth.slice(:provider, :uid)
+    user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+    user_params[:google_picture_url] = auth.info.image
+    user_params[:token] = auth.credentials.token
+    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+    user_params = user_params.to_h
+    # data = auth.info ...
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    user ||= User.find_by(email:auth.info.email)
+    unless user
+        user = User.new(user_params)
+        user.password = Devise.friendly_token[0,20]
+        user.save
+    end
+    user
+  end
 end
